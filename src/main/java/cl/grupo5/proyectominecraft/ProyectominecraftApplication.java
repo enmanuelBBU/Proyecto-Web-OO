aafedb067e6b969bc9f87159188b29c0a0631021
faraneda <felipearanedaarias@gmail.com>
2026-09-04 11:59:36 -0400

    proyecto inicial

---
+++ src/main/java/cl/grupo5/proyectominecraft/ProyectominecraftApplication.java
@@ -0,0 +1,13 @@
+package cl.grupo5.proyectominecraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectominecraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectominecraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/grupo5/proyectominecraft/ProyectominecraftApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.grupo5.proyectominecraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectominecraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
