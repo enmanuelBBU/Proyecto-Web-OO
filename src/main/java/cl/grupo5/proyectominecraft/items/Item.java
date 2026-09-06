e47b6e40751b8262d6621dab1aba4ebeebe819a4
enmanuelBBU <enmanuel.barrera2201@alumnos.ubiobio.cl>
2026-09-06 18:00:02 -0300

    feat: add recipe fields to Item

--- src/main/java/cl/grupo5/proyectominecraft/items/Item.java
+++ src/main/java/cl/grupo5/proyectominecraft/items/Item.java
@@ -3,6 +3,8 @@ package cl.grupo5.proyectominecraft.items;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Item {
   private String id;
@@ -10,4 +12,7 @@ public class Item {
   private String nombre;
   private String categoria;
   private boolean esMateriaPrima;
+  private String fullId;
+  private List<String> recetaMatriz;
+  private List<Ingrediente> ingredientesParaCalculo;
 }
